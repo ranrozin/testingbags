2b92d216d56cf111b413a54655574a8b37a17348
ranrozin <ran.rozin@gmail.com>
2014-10-21 14:09:44 +0300

    last pc changes, done before the mac ones

---
+++ web_service.py
@@ -0,0 +1,29 @@
+﻿import webapp2
+import os
+import models
+import json
+import services
+import constants
+import logging
+from google.appengine.ext import ndb
+from google.appengine.ext.ndb import metadata
+from gaesessions import get_current_session
+from google.appengine.ext.webapp import template
+
+class 
+
+
+app = webapp2.WSGIApplication(
+    	[
+			('/createID?', createIDHandler),
+			('/logout?', logoutHandler),
+			('/new?', newHandler),
+			('/submit?', submitcardHandler),
+			('/save?', savecardHandler),
+			('/search?', searchcardHandler),
+			('/get_card?', getcardHandler),
+			('/getMobileCard/([^/]+)?', getCardForMobileHandler),
+			('/register?', registrationHandler)
+    	],
+                                         debug=True) 		
+
